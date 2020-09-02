d630bb72d8afafc041c6771a303a541c8024b9ac
magnuen2k <maen030@student.kristiania.no>
2020-09-02 13:27:56 +0200

    added test for multiple parameters - failing

--- PGR-203-AdvancedJava/Oppgaver/3/httpclient/src/main/java/no/kristiania/QueryString.java
+++ PGR-203-AdvancedJava/Oppgaver/3/httpclient/src/main/java/no/kristiania/QueryString.java
@@ -2,11 +2,20 @@ package no.kristiania;
 
 public class QueryString {
 
-    public QueryString(String queryString){
+    private final String parameterValue;
+    private final String parameterName;
 
+    public QueryString(String queryString){
+        int pos = queryString.indexOf('=');
+        parameterName = queryString.substring(0, pos);
+        parameterValue = queryString.substring(pos + 1);
     }
 
     public Object getParameter(String status) {
-        return "200";
+        if(status.equals(parameterName)) {
+            return parameterValue;
+        } else {
+            return null;
+        }
     }
 }
--- PGR-203-AdvancedJava/Oppgaver/3/httpclient/src/test/java/no/kristiania/httpQueryStringTest.java
+++ PGR-203-AdvancedJava/Oppgaver/3/httpclient/src/test/java/no/kristiania/httpQueryStringTest.java
@@ -3,6 +3,7 @@ package no.kristiania;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class httpQueryStringTest {
     @Test
@@ -15,5 +16,18 @@ public class httpQueryStringTest {
     void shouldRetrieveStatusCode_401() {
         QueryString queryString = new QueryString("status=401");
         assertEquals("401", queryString.getParameter("status"));
+    }
+
+    @Test
+    void shouldReturnNullForMissingParameters() {
+        QueryString queryString = new QueryString("body=hello");
+        assertNull(queryString.getParameter("status"));
+    }
+
+    @Test
+    void shouldSupportMultipleParameters() {
+        QueryString queryString = new QueryString("status=200&body=Hello");
+        assertEquals("200", queryString.getParameter("status"));
+        assertEquals("Hello", queryString.getParameter("Hello"));
     }
 }
